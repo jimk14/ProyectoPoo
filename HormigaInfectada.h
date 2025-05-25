de5b2e5f95e5bcb3baa865c7405322ff145c24fd
Cristiangarcia04 <73946418+jimk14@users.noreply.github.com>
2025-05-25 10:25:16 -0500

    Update HormigaInfectada.h

--- HormigaInfectada.h
+++ HormigaInfectada.h
@@ -1,8 +1,26 @@
 #ifndef HORMIGAINFECTADA_H
 #define HORMIGAINFECTADA_H
 
+#include "Hormiga.h"
+#include <SFML/Graphics.hpp>
+#include <string>
+#include <vector>
 
+class HormigaInfectada : public Hormiga {
+private:
+    std::vector<sf::Texture> texturasInfectada;
+    int estadoActual;
+    float tiempoCambio;
+    sf::Clock reloj;
 
+public:
+    HormigaInfectada(int vitalidad, sf::Vector2f posicion);
 
+    void cargarTexturas();
+    void cambiarEstado();
+    void mover();
+    void atacar();
+    void actualizarSprite();
+};
 
-#endif //HORMIGAINFECTADA_H
+#endif
