e50995d1a2388dc93b9be1705948c8f646c58bc1
Cristiangarcia04 <73946418+jimk14@users.noreply.github.com>
2025-05-26 12:25:37 -0500

    inclusion ray

--- main.cpp
+++ main.cpp
@@ -1,27 +1,29 @@
 #include <SFML/Graphics.hpp>
-#include "Ray.cpp"
+#include "Ray.h"
+#include "MapaDeJuego.h"
 
-
-int main()
-{
-
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "My window");
+int main() {
+    sf::RenderWindow window(sf::VideoMode({1900, 1000}), "Ray: El Explorador");
     window.setFramerateLimit(60);
-    sf::Texture texture;
-    sf::RectangleShape rectangle({100,150});
-    rectangle.setTexture(&texture);
-    rectangle.setFillColor(sf::Color::White);
-    rectangle.setPosition(sf::Vector2f(400, 300));
+
+    Ray ray;
+    MapaDeJuego mapa;
 
     while (window.isOpen()) {
-        while (const std::optional event = window.pollEvent())
-        {
+        while (const std::optional event = window.pollEvent()) {
             if (event->is<sf::Event::Closed>())
                 window.close();
         }
 
-        window.draw(rectangle);
+        ray.manejarEntrada();
+        ray.actualizar();
+
+        mapa.actualizar(ray);        // El mapa cambia de fondo si Ray se mueve
+        window.clear();
+        mapa.dibujar(window);
+        ray.dibujar(window);
         window.display();
     }
+
     return 0;
 }
